b18a7edabea403f99401924442680385b748fedf
Ana7K <anakitesashvili4@gmail.com>
2026-05-26 23:52:19 +0400

    commit

--- Lec5/hw.js
+++ Lec5/hw.js
@@ -43,24 +43,82 @@ function checkMessage() {
 
 // 4.დაწერე ფუქნცია რომელიც პარამეტრად მიიღებს API და დააბრუნებს ამ API-ში
 // მყოფ  4 - users. https://jsonplaceholder.typicode.com/users დაწერე ორივენაირად than/catch & async/await
+async function fetchAPI(API) {
+  let res = await fetch(API);
+  let data = await res.json();
+  return data.slice(0, 4);
+}
 
+function fetchAPIThenCatch(API) {
+  return fetch(API)
+    .then((res) => res.json())
+    .then((data) => data.slice(0, 4))
+    .catch((err) => {
+      console.error("Fetch error:", err);
+      throw err;
+    });
+}
+
+fetchAPI("https://jsonplaceholder.typicode.com/users");
 
 // 5) დააწყვილე reduce-თი ცალკე ვისი ასაკიც მეტია 10 ზე და ვისი ასაკიც ნაკლებია 20
-// let people = [
-//   { name: "Giorgi", age: 25 },
-//   { name: "Nika", age: 15 },
-//   { name: "Mariam", age: 30 },
-//   { name: "Luka", age: 18 }
-// ];
+let people = [
+  { name: "Giorgi", age: 25 },
+  { name: "Nika", age: 15 },
+  { name: "Mariam", age: 30 },
+  { name: "Luka", age: 18 },
+];
+
+let result = people.reduce(
+  (tot, curr) => {
+    if (curr.age > 10) {
+      tot.moreThan10.push(curr);
+    }
+    if (curr.age < 20) {
+      tot.lessThan20.push(curr);
+    }
+    return tot;
+  },
+  { moreThan10: [], lessThan20: [] },
+);
+
+// console.log(result);
 
 // 6. დაწერე ფუნქცია რომელიც მიიღებს ორ რიცხვს და callback-ს. თუ პირველი მეტია მეორეზე გაუშვი callback და დაუბეჭდე "მეტია", სხვა შემთხვევაში "ნაკლები ან ტოლია".
+function compareNums(num1, num2, callback) {
+  if (num1 > num2) {
+    callback();
+  } else {
+    console.log("num1 is less or equal to num2");
+  }
+}
+
+function message() {
+  console.log("num1 is more than num2");
+}
+
+compareNums(1, 7, message);
 
 // 7.დაწერე reduce, რომელიც დააჯგუფებს - ცალკე 20-ზე მეტ ფასიან რიცხვებს და
 // ცალკე 20-ზე ნაკლები ან ტოლი ფასიანი ნივთები
-// let products = [
-//   { name: "Mouse", price: 15 },
-//   { name: "Keyboard", price: 45 },
-//   { name: "USB Cable", price: 7 },
-//   { name: "Headphones", price: 29.9 },
-//   { name: "Webcam", price: 52 }
-// ];
+let products = [
+  { name: "Mouse", price: 15 },
+  { name: "Keyboard", price: 45 },
+  { name: "USB Cable", price: 7 },
+  { name: "Headphones", price: 29.9 },
+  { name: "Webcam", price: 52 },
+];
+let grouped = products.reduce(
+  (tot, curr) => {
+    if (curr.price > 20) {
+      tot.moreThan20.push(curr);
+    }
+    if (curr.price <= 20) {
+      tot.lessThan20.push(curr);
+    }
+    return tot;
+  },
+  { moreThan20: [], lessThan20: [] },
+);
+
+console.log(grouped);
--- Lec6/hw.js
+++ Lec6/hw.js
@@ -1,14 +1,28 @@
-// 1) function block(){
-//     for(let i = 1 ;i <10000000000;i++){}
-// }
-
-// console.log("one")
-// block()
-// console.log("two")
-// იპოვე გამოსავალი როგორ შეიძლება გაეშვას ჯერ  console.log("one") console.log("two") შემდეგ ფუნქცია
+// 1)იპოვე გამოსავალი როგორ შეიძლება გაეშვას ჯერ  console.log("one") console.log("two") შემდეგ ფუნქცია
 // აუცილებელია გამოიყენო ფრომისი
+function block() {
+  return new Promise((resolve) => {
+    for (let i = 1; i < 10000000000; i++) {}
+    resolve();
+  });
+}
+
+// console.log("one");
+// block();
+// console.log("two");
 
 // 2)ორი პრომისი შექმენი (ერთმა დაარესოლვოს, ერთმა დაარეჯექთოს) და ორივე შემთხვევა დაამუშავე then/catch-ით  ცალცალკეც და “ჯგუფურადაც”  - ჯგუფურად Allsetteld გამოიყენე.
+let myPromise1 = new Promise((res, rej) => {
+  res("first promise responded");
+});
+let myPromise2 = new Promise((res, rej) => {
+  rej("second promise rejected");
+});
+
+myPromise1.then((res) => console.log("Promise1:", res));
+myPromise2
+  .then((res) => console.log("Promise2:", res))
+  .catch((err) => console.log("Promise2 error:", err));
 
 // 3)შექენი 4 პრომისი (ზოგი resolve, ზოგი reject). დააბრუნე მარტო პირველი დარესოლვებული
 
@@ -16,6 +30,6 @@
 
 // 5) შექმენი 5 ფრომისი და გაფილტრე ეს ფრომისები დააბრუნე ამრტო წარუმატებლები
 
-// 6)api1 = https://jsonplaceholder.typicode.com/users
-// api2 = https://jsonplaceholder.typicode.com/posts
-// გაუშვი ეს ორი API ერთდროულად
+// 6)// გაუშვი ეს ორი API ერთდროულად
+api1 = "https://jsonplaceholder.typicode.com/users";
+api2 = "https://jsonplaceholder.typicode.com/posts";
